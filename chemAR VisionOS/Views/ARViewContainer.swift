ab5d910545838ca397944235981e274c66ca97b8
Neeraj Shetkar <neerajshetkar@gmail.com>
2023-12-12 11:10:12 +0530

    Electron is revolving but very absuptly. Need to adjust motion and position.

--- chemAR VisionOS/Views/ARViewContainer.swift
+++ chemAR VisionOS/Views/ARViewContainer.swift
@@ -10,39 +10,52 @@ import RealityKit
 import RealityKitContent
 
 struct ARViewContainer: View {
-    
     @State private var electronRotations: [Double] = []
     @Environment(\.colorScheme) var colorScheme
-    let animationTimer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
+    @State private var atom: Entity?
+    let animationTimer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     
     var info: ElementInfo
+    
     var body: some View {
         RealityView { content in
+            if let scene = try? await Entity(named: "ProtonComponent", in: realityKitContentBundle) {
+                content.add(scene)
+                self.atom = scene
+            }
             if let scene = try? await Entity(named: "ElectronComponent", in: realityKitContentBundle) {
                 content.add(scene)
+                self.atom = scene
+                setupRotations()
             }
         }
-    }
-    
-    // Rotation angle
-    private func setupRotations() {
-        electronRotations = Array(repeating: 0.0, count: info.shells.count)
-    }
-    
-    // Text position
-    private func getTextPosition(for index: Int, in size: CGSize, radius: CGFloat) -> CGPoint {
-            let angle = 360.0 / Double(info.shells.count) * Double(index)
-            let xPos = cos(angle * .pi / 180) * Double(radius) + Double(size.width / 2)
-            let yPos = sin(angle * .pi / 180) * Double(radius) + Double(size.height / 2)
-            return CGPoint(x: xPos, y: yPos)
+        .onAppear {
+            setupRotations()
         }
+        .onReceive(animationTimer) { _ in
+            animateRotations()
+        }
+    }
     
+    private func setupRotations() {
+        guard let atom = atom else { return }
+        electronRotations = Array(repeating: 0.0, count: info.shells.count)
+        animateRotations()
+    }
+
     private func animateRotations() {
+        guard let atom = atom else { return }
         for i in 0..<electronRotations.count {
-            electronRotations[i] += 2.0 / Double(i + 1) // Adjust the rotation speed here
+            let rotationAngle = 2.0 / Float(i + 1) // Adjust the rotation speed here
+            electronRotations[i] += Double(rotationAngle)
+            let rotation = SIMD3<Float>(0, 1, 0) // Rotate around Y-axis
+            var transform = Transform()
+            transform.rotation = simd_quatf(angle: Float(electronRotations[i]), axis: rotation)
+            atom.transform = transform
         }
     }
 }
+
 
 #Preview {
     ARViewContainer(info: mockData)
